3d555e1e44ce0bfe59816d03654bdced2fb069aa
SANJAY B <75837200+imsanju02k@users.noreply.github.com>
2021-09-25 19:30:31 +0530

    Add files via upload

---
+++ coins.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        
+        if(amount<1)
+            return 0;
+        int [] dp =new int [amount+1];
+        int sum=0;
+        while (++sum <= amount){
+            int min =-1;
+            for(int coin :coins){
+                if(sum >= coin && dp[sum-coin]!=-1)
+                {
+                    int temp = dp[sum-coin]+1;
+                    if(min < 0 || temp < min)
+                        min=temp;
+                    
+                }
+            }
+             dp[sum]=min;
+            
+        }
+        return dp[amount];
+    }
+}
